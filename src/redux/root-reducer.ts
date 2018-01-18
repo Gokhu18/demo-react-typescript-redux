087427bb49937d65d3598de961ce6ae0d24c0e48
Rob Brennan <rob@therobbrennan.com>
2018-01-18 01:46:20 -0800

    Minor clean up of root reducer

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -3,11 +3,9 @@ import { RootState } from '../types/index';
 
 // Reducers
 import { routerReducer as router } from 'react-router-redux';
-// import { reducer as counters, State as CountersState } from '@src/redux/counters';
-// import { reducer as todos, State as TodosState } from '@src/redux/todos';
+import { enthusiasm } from '../reducers/index';
 
-// Actions
-// import { RootAction } from '@src/redux';
-// import { RootAction } from './root-action';
-
-export const rootReducer = combineReducers<RootState>({router});
+export const rootReducer = combineReducers<RootState>({
+  router,
+  enthusiasm
+});
